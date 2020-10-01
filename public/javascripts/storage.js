7dcfe4f17a7a6b9e0fee6055489bbf11186ce839
Adrian G <cgdoerpen@gmail.com>
2020-10-01 22:08:13 +0200

    added function to create items in localstorage for website

---
+++ public/javascripts/storage.js
@@ -0,0 +1,14 @@
+window.onload = init;
+function init() {
+    document.getElementById("send-button").addEventListener("click", savePage);
+}
+
+
+function savePage(data) {
+    let title = document.getElementById("title").value;
+    let subtitle = document.getElementById("subtitle").value;
+    let content = document.getElementById("content").value;
+    localStorage.setItem(title + "-title", title);
+    localStorage.setItem(title + "-subtitle", subtitle);
+    localStorage.setItem(title + "-content", content);
+}
